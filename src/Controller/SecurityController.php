d3a5069d615a1c5c3405a533a2755a73c216f9a7
Timothe Raynaud <raynaud.timothe@gmail.com>
2024-01-05 12:06:13 +0100

    :construction: Big update to symfony 6.4 and php 8.1

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        if ($error){
+            $this->addFlash('error', 'Login ou mot de passe incorrect.');
+        }
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/login_check', name: 'login_check')]
+    public function loginCheck(): void
+    {
+        // This code is never executed.
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[Route(path: '/logout', name: 'user_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route(path: '/users', name: 'user_list')]
+    public function listAction(UserRepository $userRepository): Response
+    {
+        return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
+    }
+
+    #[Route(path: '/users/create', name: 'user_create')]
+    public function createAction(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route(path: '/users/{id}/edit', name: 'user_edit')]
+    public function editAction(User $user, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
